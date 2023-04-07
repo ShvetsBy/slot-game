541cd0261bf2239d800adb096b0405b553ea4104
Ivan <shvets.ivan@gmail.com>
2023-04-07 10:57:08 +0300

    TS init

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Narcos</h1>
+      <h2 className="sub-heading">The slot game</h2>
+      <p className="legal-notice">
+        You must be above the legal gambling age in order to try our demo games
+        on this website. For real money betting, please refer to our licenced
+        casino partners that are advertised.
+      </p>
+    </div>
+  );
+}
+
+export default App;
