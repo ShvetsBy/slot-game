f6296bdca16ba557b328bf3136372222a2855901
Ivan Shvets <ivanshvets@Ivans-MacBook-Pro.local>
2023-04-28 22:08:27 +0300

    update spin button

--- src/components/controlPanel/spin.tsx
+++ src/components/controlPanel/spin.tsx
@@ -3,14 +3,16 @@ import React, { KeyboardEventHandler } from 'react'
 import './controlPanel.css'
 import { gsap } from 'gsap'
 import Drum from '../../assets/img/drum.png'
+import rawDrum from '../../assets/img/drum-noborder.png'
+import fireCircle from '../../assets/img/fire-rounded.png'
 
 export function Spin() {
   const clickHandler = () => {
     gsap.fromTo('#drum', { rotation: 0 }, { rotation: 360, repeat: 4, ease: 'none' })
   }
 
-  document.addEventListener('keydown', function (event) {
-    if (event.code === 'Space') {
+  document.addEventListener('keydown', (e) => {
+    if (e.code === 'Space') {
       clickHandler()
     }
   })
@@ -18,7 +20,9 @@ export function Spin() {
   return (
     // eslint-disable-next-line jsx-a11y/click-events-have-key-events
     <div className="spin-wheel-wrapper" onClick={clickHandler}>
-      <img src={Drum} alt="spin wheel" className="spin-wheel" id="drum" />
+      {/* <img src={Drum} alt="spin wheel" className="spin-wheel" id="drum" /> */}
+      <img src={fireCircle} alt="spin wheel" className="spin-fire" />
+      <img src={rawDrum} alt="spin wheel" className="spin-wheel" id="drum" />
     </div>
   )
 }
