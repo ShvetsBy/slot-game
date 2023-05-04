6cd65fabb9cebb44bd7d2bd35732908d497281f8
Ivan Shvets <ivanshvets@Ivans-MacBook-Pro.local>
2023-05-04 12:05:31 +0300

    remove redundant game wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './App.css'
 
 import { Provider } from 'react-redux'
-import { GameWrapper } from './components/gameWrapper/gameWrapper'
+
 import { GameField } from './components/reels/gameField'
 import { ControlPanel } from './components/controlPanel/controlPanel'
 import { InfoBar } from './components/infoBar/infoBar'
@@ -18,13 +18,13 @@ function App() {
           You must be above the legal gambling age in order to try our demo games on this website.
           For real money betting, please refer to our licenced casino partners that are advertised.
         </p>
-        <GameWrapper>
+        <div className="game-wrapper" id="reels">
           <GameField />
           <div>
             <ControlPanel />
             <InfoBar />
           </div>
-        </GameWrapper>
+        </div>
       </div>
     </Provider>
   )
